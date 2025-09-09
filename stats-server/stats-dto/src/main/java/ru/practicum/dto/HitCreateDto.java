08ad08c32b9ec8b0dd416216e4c23e66feafe8b6
VitalyL <vetallrssn@yandex.ru>
2025-09-09 20:06:46 +0300

    Добавить HitCreateDto и ResponseStatsDto

---
+++ stats-server/stats-dto/src/main/java/ru/practicum/dto/HitCreateDto.java
@@ -0,0 +1,30 @@
+package ru.practicum.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+public record HitCreateDto(
+        @NotBlank
+        String name,
+
+        @NotBlank
+        String app,
+
+        @NotBlank
+        String uri,
+
+        @NotBlank
+        String ip,
+
+        @NotNull
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime timestamp
+) {
+    @Builder(toBuilder = true)
+    public HitCreateDto {
+    }
+}
---
+++ stats-server/stats-dto/src/main/java/ru/practicum/dto/ResponseStatsDto.java
@@ -0,0 +1,12 @@
+package ru.practicum.dto;
+
+import lombok.Builder;
+
+public record ResponseStatsDto(
+        String app,
+        String uri,
+        Long hits) {
+    @Builder(toBuilder = true)
+    public ResponseStatsDto {
+    }
+}
