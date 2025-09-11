f35ef6eed2539532caadc7a246abcc0b411565dd
VitalyL <vetallrssn@yandex.ru>
2025-09-11 13:25:46 +0300

    Убрать  @JsonFormat из RequestStats

--- stats-server/stats-dto/src/main/java/ru/practicum/dto/RequestStats.java
+++ stats-server/stats-dto/src/main/java/ru/practicum/dto/RequestStats.java
@@ -1,16 +1,12 @@
 package ru.practicum.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record RequestStats(
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime start,
-
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime end,
         List<String> uris,
         Boolean unique
