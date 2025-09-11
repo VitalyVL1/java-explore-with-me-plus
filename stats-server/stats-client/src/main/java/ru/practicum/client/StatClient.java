ae1666afec4cbe9c0438ee39d210129a20d413ae
VitalyL <vetallrssn@yandex.ru>
2025-09-11 14:33:51 +0300

    Добавить clint

---
+++ stats-server/stats-client/src/main/java/ru/practicum/client/StatClient.java
@@ -0,0 +1,13 @@
+package ru.practicum.client;
+
+import ru.practicum.dto.HitCreateDto;
+import ru.practicum.dto.RequestStats;
+import ru.practicum.dto.ResponseStatsDto;
+
+import java.util.List;
+
+public interface StatClient {
+    void hit(HitCreateDto dto);
+
+    List<ResponseStatsDto> get(RequestStats request);
+}
---
+++ stats-server/stats-client/src/main/java/ru/practicum/client/StatClientRestImpl.java
@@ -0,0 +1,79 @@
+package ru.practicum.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import ru.practicum.dto.HitCreateDto;
+import ru.practicum.dto.RequestStats;
+import ru.practicum.dto.ResponseStatsDto;
+
+import java.net.URI;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@Slf4j
+@Component
+public class StatClientRestImpl implements StatClient {
+
+    private final String baseUrl;
+    private final DateTimeFormatter dateTimeFormatter;
+    private final RestClient restClient;
+
+    public StatClientRestImpl(
+            @Value("${stats.service.url}") String baseUrl,
+            @Value("${stats.date_time.format}") String dateTamePattern
+    ) {
+        this.baseUrl = baseUrl;
+        this.dateTimeFormatter = DateTimeFormatter.ofPattern(dateTamePattern);
+        this.restClient = RestClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+
+    @Override
+    public void hit(HitCreateDto dto) {
+        try {
+            restClient.post()
+                    .uri("/hit")
+                    .contentType(APPLICATION_JSON)
+                    .body(dto)
+                    .retrieve()
+                    .toBodilessEntity();
+        } catch (Exception e) {
+            log.warn("Failed to send hit to stats service", e);
+            throw e;
+        }
+    }
+
+    @Override
+    public List<ResponseStatsDto> get(RequestStats requestStats) {
+        try {
+            URI uri = UriComponentsBuilder.fromUri(URI.create(baseUrl))
+                    .path("/stats")
+                    .queryParam("start", requestStats.start().format(dateTimeFormatter))
+                    .queryParam("end", requestStats.end().format(dateTimeFormatter))
+                    .queryParam("unique", requestStats.unique() != null ?
+                            requestStats.unique() :
+                            false)
+                    .queryParamIfPresent("uris", Optional.ofNullable(requestStats.uris()))
+                    .build()
+                    .toUri();
+
+            return restClient.get()
+                    .uri(uri)
+                    .accept(APPLICATION_JSON)
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {
+                    });
+        } catch (Exception e) {
+            log.warn("Failed to get stats from stats service", e);
+            throw e;
+        }
+    }
+}
